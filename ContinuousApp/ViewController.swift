eb2fefe766878bf3521a2add7abc53c361c85243
Paul Peelen <paul@paulpeelen.com>
2018-09-27 13:44:23 +0200

    Add the initial empty xcode project

---
+++ ContinuousApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContinuousApp
+//
+//  Created by Paul Peelen on 2018-09-27.
+//  Copyright © 2018 AppTrix AB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
